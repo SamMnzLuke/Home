d33b35bb51d5dedc5153600265f728eb515e39ee
SamMnzLuke <124309381+SamMnzLuke@users.noreply.github.com>
2023-02-04 13:18:32 -0600

    Update links.js

--- js/links.js
+++ js/links.js
@@ -1,4 +1,4 @@
-particlesJS("particles-js" {
+particlesJS("particles-js", {
     particles: {
         number: { value: 6, density: { enable: true, value_area: 1000 } },
         color: { value: "#ebebeb" },
